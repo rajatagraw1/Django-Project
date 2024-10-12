9d3f0a999222f60b42a0fbe6e125c41626d0544f
Rajat Agrawal <96256504+rajatagraw1@users.noreply.github.com>
2024-10-12 14:15:25 +0530

    Update settings.py

--- app_cost_calculator/app_cost_calculator/settings.py
+++ app_cost_calculator/app_cost_calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4w1%o7!k1c=y9#26no+6lx82uu=yd*z=iuz3e0hjo=qko*)m4p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
